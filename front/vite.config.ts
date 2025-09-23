343fb8751f756269c36bb258b59d1dd18e78c0e4
Lancelot de Ferrière <wraitii@gmail.com>
2025-09-23 11:11:23 +0200

    Initial commit

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,24 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    esbuildOptions: { target: "esnext" },
-    exclude: ["@noir-lang/noirc_abi", "@noir-lang/acvm_js"],
-  },
-  resolve: {
-    alias: {
-      buffer: 'buffer',
-    },
-  },
-  define: {
-    global: 'globalThis',
-  },
-  server: {
-    fs: {
-      allow: ['../..'],
-    },
-  },
+    plugins: [vue(), tailwindcss()],
 });
